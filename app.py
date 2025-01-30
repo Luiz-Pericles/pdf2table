90e7842267327a10d22d49efc25d463bdcee2b33
Luiz Péricles <136658343+Luiz-Pericles@users.noreply.github.com>
2025-01-30 14:30:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import pdfplumber
 import pandas as pd
-import re
+import streamlit as st
+import io
 
-caminho_do_arquivo = "TR_exemplo.pdf"
+
+
+
 
 def extrair_tabelas(arquivo:str, paginas:list):
     todas_as_tabelas = []
@@ -13,8 +16,9 @@ def extrair_tabelas(arquivo:str, paginas:list):
             tabela = pagina_selecionada.extract_table(table_settings={"vertical_strategy": "lines_strict", 
                                                                       "horizontal_strategy": "lines_strict"})
             if tabela:
-                todas_as_tabelas.extend(tabela[1:] if todas_as_tabelas else tabela)  # Pular cabeçalho após a primeira
-
+                todas_as_tabelas.extend(tabela)  # Pular cabeçalho após a primeira
+            else:
+                print(f'Não foi encontrado tabela na página: {numero_da_pagina+1}')
     return todas_as_tabelas
 
 def remover_caracteres_ilegais(value):
@@ -23,21 +27,64 @@ def remover_caracteres_ilegais(value):
         return ''.join(ch for ch in value if ch.isprintable())
     return value
 
+@st.cache_data
 def tabelas_para_xlsx(tabelas_extraidas):
     if tabelas_extraidas:
         cabecalho = tabelas_extraidas[0]
         df = pd.DataFrame(tabelas_extraidas[1:], columns=cabecalho)
-        df = df.applymap(remover_caracteres_ilegais)
+        df = df.map(remover_caracteres_ilegais)
         print(df)
         df.to_excel("tabela_consolidada.xlsx", index=False)
         print("Tabela consolidada salva em 'tabela_consolidada.xlsx'")
     else:
         print("Nenhuma tabela encontrada.")
+    return df
 
+
+
+
+
+# Criar interface streamlit
+
+st.write("# App *PDF2TABLE* :alien:")
+    # Entrada de dados do usuário: 
 # Especificar as páginas que contêm as tabelas
-paginas_com_tabelas = [ 0, 1, 2, 3 ]  # Índices zero para páginas 1, 2 e 3
+with st.form('my_form'):
+    
+    # ultima_pag_com_tabela = int(st.selectbox("Qual o número da página com o fim da tabela: ", ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10')))
+    ultima_pag_com_tabela = int(st.number_input("Qual o número da página com o fim da tabela: ", min_value=1, max_value=20, step=1))
+    submitted = st.form_submit_button('Salvar')
+st.write(ultima_pag_com_tabela)
+# Criar uma lista com as páginas que possui a tabela
+paginas_com_tabelas = list()
+for i in range(0, ultima_pag_com_tabela):
+    paginas_com_tabelas.append(i)
 
-tabelas = extrair_tabelas(caminho_do_arquivo, paginas_com_tabelas)
-print(tabelas)
-print(type(tabelas))
-tabelas_para_xlsx(tabelas) 
+# Para o usuário selecionar o arquivo pdf
+st.subheader("Selecione ou arraste o Termo de Referência em PDF")
+uploaded_file = st.file_uploader("suba o arquivo pdf", type=['pdf'], label_visibility="hidden")
+if uploaded_file is not None:
+        
+    # Função para extrair a tabela do pdf, retorna um DF
+    tabelas = extrair_tabelas(uploaded_file, paginas_com_tabelas)
+    # Função para transformar o DF em arquivo xlsx
+    tabela_tratada = tabelas_para_xlsx(tabelas) 
+
+    # Escrever a tabela extraida na tela
+    st.write("Segue a extração da tabela: ")
+    st.write(tabela_tratada)
+    
+    # Botão de download do excel para streamlit
+    buffer = io.BytesIO()
+    with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+        tabela_tratada.to_excel(writer, sheet_name='Tabela_do_TR')
+        # Fechar o ExcelWriter e devolver o arquivo Excel para o buffer
+        writer.close()
+        
+        st.download_button(label="Baixe a tabela de itens em Excel", data=buffer, file_name="Itens_do_TR.xlsx", mime="application/vnd.ms-excel")
+
+else:
+    st.info(' Faça o upload de um Termo de Referência em PDF')
+
+st.write("## FIM")
+
